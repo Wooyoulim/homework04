d58addcb47b5012ea517c920e49e60a0388798f8
Wooyoulim <y01093316417@gmail.com>
2024-05-13 21:23:19 +0900

    리액트 숙제1

---
+++ src/assets/api/data3.js
@@ -0,0 +1,23 @@
+const con4 = [
+    {
+        id: 1,
+        imgurl: './images/content/con4-icon(1).png',
+        kor: ' CEO 인사말',
+        eng: 'CEO’s Greetings',
+    },
+    {
+        id: 2,
+        imgurl: './images/content/con4-icon(2).png',
+        kor: '경영이념',
+        eng: 'Management Philosophy',
+    },
+    {
+        id: 3,
+        imgurl: './images/content/con4-icon(3).png',
+        kor: '관계사',
+        eng: 'Business Overview',
+    },
+];
+
+// export { con4};
+export default con4;
